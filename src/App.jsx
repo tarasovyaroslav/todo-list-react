3fd72cfe4e5c9bcdc31f85abe3cdc7fb84885c50
tarasovyaroslav <tarasov.fortech@gmail.com>
2022-07-14 14:16:32 +0300

    init project & configure webpack

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    return <div>Some text</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    // Entry point of app
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    // Output files
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: production
+            ? '[name].[contenthash].js'
+            : '[name].js',
+        clean: true, // clear dist folder
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: production
+                ? '[name].[contenthash].css'
+                : '[name].css',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /node_modules/,
+                use: [
+                    production
+                        ? MiniCssExtractPlugin.loader
+                        : 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: !production,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: !production,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: !production,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.sass', '.scss'],
+    },
+
+    devServer: {
+        port: 3000,
+        hot: true,
+    },
+    mode: production ? 'production' : 'development',
+    target: production ? 'browserslist' : 'web',
+};
